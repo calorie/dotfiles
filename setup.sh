c246e8bb3a2819239b19813582b96baebf24b4c5
Tomohito Ozaki <ozaki@yuroyoro.com>
2012-01-12 17:18:49 +0900

    modified setup.sh.

--- setup.sh
+++ setup.sh
@@ -8,17 +8,4 @@ ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/import.scala $HOME/import.scala
 ln -s $HOME/dotfiles/.vrapperrc $HOME/.vrapperrc
-IFS=:
-GIT=false
-for d in $PATH
-do test -x $d/git && GIT=true
-done
-if $GIT
-then
-  echo "git found!"
-  git config --global user.name "yuroyoro"
-  git config --global user.email "ozaki@yuroyoro.com"
-  git config --global core.excludesfile "$HOME/.gitignore"
-  ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-else echo "no git"
-fi
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
