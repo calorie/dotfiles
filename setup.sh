23394bdff109cf41adb4683c4ec3df27f775ea3c
Tomohito Ozaki <ozaki@yuroyoro.com>
2012-02-09 21:57:54 +0900

    iTerm2 + xterm-256 + tmux環境に移行した。

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vrapperrc import.scala)
+DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vrapperrc import.scala .tmux.conf .dir_colors)
 
 for file in ${DOT_FILES[@]}
 do
