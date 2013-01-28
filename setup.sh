b5d80935779bd648b55667857031f33b2634be94
YuuShigetani <s2g4t1n2@gmail.com>
2013-01-28 10:44:56 +0900

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ORIGIN=$(pwd)
 
-DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc)
+DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vrapperrc import.scala .tmux.conf .tmux-powerlinerc .dir_colors .rdebugrc)
 
 for file in ${DOT_FILES[@]}
 do
