cf3e5119f1e5e3deb40114f2c9363b182963fb2f
YuuShigetani <s2g4t1n2@gmail.com>
2013-04-29 17:56:34 +0900

    uninstall.sh

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-setup
+dotfiles
 ========
+## setup
 
 ```
 $ git clone git://github.com/YuuShigetani/dotfiles.git ~/dotfiles
-$ cd ~/dotfiles
-$ ./setup.sh
+$ ~/dotfiles/setup.sh
+```
+
+## uninstall
+
+```
+$ . ~/dotfiles/uninstall.sh
 ```
 
---
+++ uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+while :
+do
+    read -p "uninstall dotfiles? y/n " yn
+    if [ $yn = "n" -o $yn = "N" ]; then
+        exit
+    elif [ $yn = "y" -o $yn = "Y" ]; then
+        break
+    else
+        continue
+    fi
+done
+
+if [ $(pwd) = $HOME/dotfiles ]; then
+    cd $HOME
+fi
+
+DOT_FILES=(
+    .zsh .zshrc .zshrc.alias
+    .zshrc.linux .zshrc.osx .ctags
+    .emacs.el .gdbinit .gemrc .gitconfig
+    .gitignore .inputrc .irbrc .sbtconfig
+    .screenrc .vimrc .vrapperrc
+    import.scala .tmux.conf .tmux-powerlinerc
+    .dir_colors .rdebugrc .tmux
+    .vim/bundle dotfiles )
+
+for file in ${DOT_FILES[@]}
+do
+    rm -rf $HOME/$file
+done
