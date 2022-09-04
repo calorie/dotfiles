c7f43000082b92289997b63f959ed874e0ebe148
Yu Shigetani <s2g4t1n2@gmail.com>
2022-09-04 23:41:38 +0900

    wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,17 @@
 local wezterm = require 'wezterm'
+
+local palette = {
+  base = '#191724',
+  overlay = '#26233a',
+  text = '#c9c8db',
+  love = '#d66B7f',
+  gold = '#f3cc95',
+  rose = '#ebbcba',
+  pine = '#31748f',
+  foam = '#9ccfd8',
+  iris = '#c4a7e7',
+}
+
 return {
   font = wezterm.font('HackGen'),
   font_size = 15.0,
@@ -7,14 +20,33 @@ return {
   color_scheme = 'Rosé Pine (base16)',
   window_decorations = 'NONE',
   enable_tab_bar = false,
+  enable_scroll_bar = false,
   use_ime = true,
   adjust_window_size_when_changing_font_size = false,
   window_close_confirmation = 'NeverPrompt',
 
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+
   colors = {
-    cursor_bg = '#c9c8db',
-    cursor_fg = '#191724',
-    cursor_border = '#c9c8db',
+    foreground = palette.text,
+    cursor_bg = palette.text,
+    cursor_fg = palette.base,
+    cursor_border = palette.text,
+    ansi = {
+      palette.overlay,
+      palette.love,
+      palette.pine,
+      palette.gold,
+      palette.foam,
+      palette.iris,
+      palette.rose,
+      palette.text,
+    },
   },
 
   keys = {
