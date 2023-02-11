90e95e8765ba299b65c0a9a4e8dee21df98a30bc
Yu Shigetani <s2g4t1n2@gmail.com>
2023-02-11 21:08:43 +0900

    nvim-tree

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -306,9 +306,18 @@ require('lazy').setup({
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
 
-      local function open_nvim_tree()
-        require('nvim-tree.api').tree.open()
+      local function open_nvim_tree(data)
+        local real_file = vim.fn.filereadable(data.file) == 1
+
+        local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+        if not real_file and not no_name then
+          return
+        end
+
+        require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
       end
+
       vim.api.nvim_create_autocmd('VimEnter', {
         nested = true,
         group = 'MyAutoCmd',
