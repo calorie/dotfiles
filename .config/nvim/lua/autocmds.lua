b8f8959f73ecfa0218a3791ca5256b258981eb6b
Yu Shigetani <s2g4t1n2@gmail.com>
2023-05-19 01:29:49 +0900

    golang

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -23,6 +23,17 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  group = 'MyAutoCmd',
+  pattern = { 'go' },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 4
+  end,
+})
+
 vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
   group = 'MyAutoCmd',
   pattern = '*.slim',
