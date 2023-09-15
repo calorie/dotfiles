b3672221fee734aa729e930e2babee18b07d008a
Yu Shigetani <s2g4t1n2@gmail.com>
2023-09-16 01:04:41 +0900

    focus

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -379,13 +379,17 @@ require('lazy').setup({
       local ignore_filetypes = { 'NvimTree' }
       local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
 
-      local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+      local augroup =
+          vim.api.nvim_create_augroup('FocusDisable', { clear = true })
 
       vim.api.nvim_create_autocmd('WinEnter', {
         group = augroup,
         callback = function(_)
-          if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-            vim.b.focus_disable = true
+          if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+          then
+            vim.w.focus_disable = true
+          else
+            vim.w.focus_disable = false
           end
         end,
         desc = 'Disable focus autoresize for BufType',
@@ -395,7 +399,9 @@ require('lazy').setup({
         group = augroup,
         callback = function(_)
           if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.b.focus_disable = true
+            vim.w.focus_disable = true
+          else
+            vim.w.focus_disable = false
           end
         end,
         desc = 'Disable focus autoresize for FileType',
