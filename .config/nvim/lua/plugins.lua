ac9a189b2a0e7d1b70d9473f767762839526487e
Yu Shigetani <s2g4t1n2@gmail.com>
2024-07-21 13:46:35 +0900

    lazy

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,13 +1,15 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+if not vim.uv.fs_stat(lazypath) then
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -34,6 +36,9 @@ local function on_attach(bufnr)
 end
 
 require('lazy').setup({
+  rocks = {
+    enabled = false,
+  },
   -- ------------------------------------
   --  Buffer
   -- ------------------------------------
