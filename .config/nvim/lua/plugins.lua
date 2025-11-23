aa50598d506f96c8f703f373c03f9d9a09f7cd1e
Yu Shigetani <s2g4t1n2@gmail.com>
2025-11-23 13:13:44 +0900

    lsp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -543,11 +543,16 @@ require('lazy').setup({
       vim.lsp.config('*', {
         capabilities = require('cmp_nvim_lsp').default_capabilities(),
         on_attach = function(client, bufnr)
+          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+
           local bufopts = { noremap = true, silent = true, buffer = bufnr }
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
           vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, bufopts)
           -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
           vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+          vim.keymap.set('n', '<leader>i', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
+          end, bufopts)
         end,
       })
 
@@ -559,6 +564,15 @@ require('lazy').setup({
             },
             staticcheck = true,
             gofumpt = true,
+            ['ui.inlayhint.hints'] = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              parameterNames = true,
+              rangeVariableType = true,
+              functionTypeParameters = true,
+            },
           },
         },
       })
