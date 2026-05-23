a0886a78424f6975659a121a801118698f9728fe
Yu Shigetani <1681408+calorie@users.noreply.github.com>
2026-05-23 23:58:27 +0900

    telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -374,9 +374,10 @@ require('lazy').setup({
 
   {
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    version = '*',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
       {
         'nvim-telescope/telescope-frecency.nvim',
         config = function()
